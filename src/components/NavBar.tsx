3185e241e5277fa4c35a28e3f7ef2d087d7edee3
Prakhar Gupta <prakhar.gupta.212003@gmail.com>
2024-07-12 14:04:07 +0530

    implemented dark mode and switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,13 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import logo from '../assets/logo.webp'
-import { px } from 'framer-motion'
+import { HStack, Image } from '@chakra-ui/react'
+import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
   return (
-    <HStack>
-        <Image src={logo} boxSize='60px'/>
-        <Text>NavBAr</Text>
+    <HStack justifyContent='space-between' padding='10px'>
+      <Image src={logo} boxSize='60px' />
+      <ColorModeSwitch />
     </HStack>
   )
 }
-
 export default NavBar
